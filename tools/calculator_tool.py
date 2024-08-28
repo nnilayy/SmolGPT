3e64f11f43ee682689dceab78b061c9bbc79716e
unknown <nnilayy.work@gmail.com>
2024-08-28 20:32:31 +0530

    Implemented logic for Calculator Tool Class

---
+++ tools/calculator_tool.py
@@ -0,0 +1,25 @@
+import nest_asyncio
+from dotenv import load_dotenv
+from langchain.agents import Tool
+from langchain_community.utilities.wolfram_alpha import WolframAlphaAPIWrapper
+
+load_dotenv()
+
+nest_asyncio.apply()
+
+class CalculatorTool:
+    def __init__(self):
+        self.wolfram_alpha = WolframAlphaAPIWrapper()
+        self.tool = Tool(
+            name="Calculator Tool",
+            func=self.invoke,
+            description="""Useful for when you want to perform any kind of mathematical calculation. 
+            You can ask to perform any kind of mathematical operation from Calculus, Arithmetic, Polynomial to any complex combination.
+            If a proper answer is not returned, Just reply to the user that the a proper answer cannot be found for the given query.
+            """
+        )
+
+    def invoke(self, query):
+        response = self.wolfram_alpha.run(query)
+        return response
+
